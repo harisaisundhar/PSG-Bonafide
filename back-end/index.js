6d23cdb22e2c3c13da3a7d787aec0229aa0d4b87
Harisai <harisaipravin@gmail.com>
2021-08-16 18:34:23 +0530

    Node setup

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/api', (req, res) => {
+	  res.status(200).send('Hello! Welcome to Bonafide API!')
+})
+
+app.listen(4000, () => {
+	  console.log(`Node app listening at http://localhost:4000`);
+});
