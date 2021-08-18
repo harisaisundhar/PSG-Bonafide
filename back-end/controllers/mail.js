c36dfe205d712a239f347eccc7cc4211840bfdec
Harisai <harisaipravin@gmail.com>
2021-08-18 18:57:12 +0530

    Bonafide:Backend: Node mailer added

--- back-end/controllers/mail.js
+++ back-end/controllers/mail.js
@@ -1,37 +1,33 @@
 "use strict";
+require("dotenv").config();
 
 const nodemailer = require("nodemailer");
 
-exports.sendMail = async () =>{
+exports.sendMail = async (req, res) =>{
   
-  let testAccount = await nodemailer.createTestAccount();
-
+  let testAccount = nodemailer.createTestAccount();
 
   let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, 
+    service : "gmail",
     auth: {
-      user: testAccount.user, 
-      pass: testAccount.pass, 
+      user: process.env.Email, 
+      pass: process.env.Pass, 
     },
   });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Sathwik Ch 👻" <sathwikchepyala99@gmail.com>', // sender address
-    to: "17pw32@psgtech.ac.in", // list of receivers
+  let info = transporter.sendMail({
+    from: process.env.Email, // sender address
+    to: process.env.SendEmail, // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
     html: "<b>Hello world?</b>", // html body
   });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  console.log("Message sent: %s", process.env.SendEmail);
+  res.status(200).json({
+              success: true,
+              data: "mail sent",
+  });
 }
 
 
